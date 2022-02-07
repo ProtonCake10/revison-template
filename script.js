e88485b98d54f53619f59b0f072bba1b1db2c837
Harvey Booth <96139190+ProtonCake10@users.noreply.github.com>
2022-02-07 10:06:21 +0000

    macbeth

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ tRating = [[1,1,1,1],[1,1],[1,1],[1,1]]
 
 // displayes the topic 1 table when activated
 function topic1() {
-	(document.getElementById("table")).innerHTML = "<tr><th>section</th><th>personal knowledge rating (1-10)</th></tr>   <td><button class='tORsButton' onclick='t1_0()'>Ad hoc networks</button></td><td> <div class='talleyStyle'><button class='f' onclick='talleyAdd(0, 0)'>+</button><div class='f' id='t00'>1</div><button class='f' onclick='talleyMin(0, 0)'>-</button></div>  </td>";
+	(document.getElementById("table")).innerHTML = "<tr><th>section</th><th>personal knowledge rating (1-10)</th></tr>   <td><button class='tORsButton' onclick='t1_0()'>Macbeth</button></td><td> <div class='talleyStyle'><button class='f' onclick='talleyAdd(0, 0)'>+</button><div class='f' id='t00'>1</div><button class='f' onclick='talleyMin(0, 0)'>-</button></div>  </td>";
 }
 
 function t1_0() {
-	(document.getElementById("descBox")).innerHTML = "<h1>Ad Hoc Networks</h1><p1>Ad hoc networks are temporary networks with 2 or more devices connected</p1>"
+	(document.getElementById("descBox")).innerHTML = "<h1>macbeth</h1><p1>Macbeth was a book written by shakspear and now teacher take it way to seriosly</p1>"
 }
 
 // displayes the topic 2 table when activated
